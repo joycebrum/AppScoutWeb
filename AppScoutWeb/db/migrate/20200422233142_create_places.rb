589827f8da21c07b85c56e85df29285a084c6466
Joyce Brum <joycebrumu.u@gmail.com>
2020-04-22 20:59:02 -0300

    fix: delete creating table whitout a scaffold

--- AppScoutWeb/db/migrate/20200422233142_create_places.rb
+++
@@ -1,7 +0,0 @@
-class CreatePlaces < ActiveRecord::Migration[6.0]
-  def change
-    create_table :places do |t|
-      t.string :place_type
-    end
-  end
-end
--- AppScoutWeb/db/migrate/20200422233318_create_scout_branches.rb
+++
@@ -1,7 +0,0 @@
-class CreateScoutBranches < ActiveRecord::Migration[6.0]
-  def change
-    create_table :scout_branches do |t|
-      t.string :description
-    end
-  end
-end
