1b62cb965c40bf7731e10db363ee1406af983d0d
Joyce Brum <joycebrumu.u@gmail.com>
2020-04-22 21:23:08 -0300

    fix: initial path redirect to game list

--- AppScoutWeb/config/routes.rb
+++ AppScoutWeb/config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :game_types
   resources :games
   resources :users
+  root to: 'games#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
