61ee1003274c32085c9e1dea2af174f6bb64f915
Joyce Brum <joycebrumu.u@gmail.com>
2020-05-18 18:06:45 -0300

    fix: convert language using i18n

--- AppScoutWeb/app/controllers/application_controller.rb
+++ AppScoutWeb/app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :set_brand
+  before_action :set_locale
 
   private
   def set_brand
     @brand = "https://img2.gratispng.com/20180402/blw/kisspng-scouting-for-boys-world-scout-emblem-boy-scouts-of-scout-5ac2bbad71bb96.9957304915227114694659.jpg"
+  end
+
+  def set_locale
+    if params[:locale]
+      cookies[:locale] = params[:locale]
+    end
+    if cookies[:locale]
+      if I18n.locale != cookies[:locale]
+        I18n.locale = cookies[:locale]
+      end
+    end
   end
 end
